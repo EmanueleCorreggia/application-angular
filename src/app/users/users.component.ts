07c88e8ebe3157c68ffb79390f5051741b4a60c8
Emanuele <emanuele.correggia86@gmail.com>
2020-07-24 12:44:46 +0200

    nfor

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
     styleUrls : ['./users.component.css']
 })
 export class UsersComponent {
-
+    title = 'Users';
+    users = [
+        {
+            name: 'Emanuele',
+            lastname: 'Correggia',
+            email: 'pippo@gmail.com',
+            fiscalcode: 'pippo78m22z444s',
+            province: 'Crotone',
+            phone: '333333333',
+            age: 34
+        },
+        {
+            name: 'Mario',
+            lastname: 'Correggia',
+            email: 'gastone@gmail.com',
+            fiscalcode: 'pippo78m22z444s',
+            province: 'Cosenza',
+            phone: '333333333',
+            age: 32
+        },
+        {
+            name: 'Giovanni',
+            lastname: 'Correggia',
+            email: 'pluto@gmail.com',
+            fiscalcode: 'pippo78m22z444s',
+            province: 'Reggio Calabria',
+            phone: '333333333',
+            age: 30
+        },
+        {
+            name: 'Marco',
+            lastname: 'Correggia',
+            email: 'topolino@gmail.com',
+            fiscalcode: 'pippo78m22z444s',
+            province: 'Roma',
+            phone: '333333333',
+            age: 28
+        }
+    ]
 }
